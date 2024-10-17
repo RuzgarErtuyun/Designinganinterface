02dab88f75dcfda1a61448021eb599e0af37a4c2
Ruzgar <ruzgar.ertuyun@hisarschool.k12.tr>
2024-10-17 13:18:47 +0300

    .

---
+++ Colorpickerapp/ColorpickerappTests/ColorpickerappTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorpickerappTests.swift
+//  ColorpickerappTests
+//
+//  Created by Ruzgar Ertuyun on 17.10.2024.
+//
+
+import Testing
+@testable import Colorpickerapp
+
+struct ColorpickerappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
